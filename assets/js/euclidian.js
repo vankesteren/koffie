43a22690844795060337dbe77344bf160393d511
etothej <erikjan.vankesteren@gmail.com>
2016-08-11 20:13:07 +0200

    math included

--- assets/js/euclidian.js
+++ assets/js/euclidian.js
@@ -10,7 +10,9 @@ var calculate = function(){
   $("#output-block").html("<h1>"+sum+"</h1>");
 }
 
+
 var euclid = function(){
+  dataset = json
   var input = [parseInt($("#var1").val()) || 0,
                parseInt($("#var2").val()) || 0,
                parseInt($("#var3").val()) || 0,
@@ -18,9 +20,10 @@ var euclid = function(){
 
   $("#output-block").html("<h1>"+JSON.stringify(input)+"</h1>");
   for (var i=0; i<json.length; i++){
-    var compare = $.map(json[i], function(el) { return(el) }).shift()
-    console.log(compare)
+    var compare = $.map(json[i], function(el) { return(el) }).slice(1)
+    dataset[i].push(Math.sqrt(math.dot(input,compare)))
   }
+  console.log(dataset)
 }
 
 
@@ -31,6 +34,8 @@ $("#var4").on("change", calculate);
 
 var json;
 var dataset;
+var out = new Array(json.length).fill(0);
 $.get("assets/JSON/euclid.txt", function(data){
   json = $.parseJSON(data);
+  dataset = json
 });
