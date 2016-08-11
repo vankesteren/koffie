32e0377ba22c1a6d1cbb8a8af5f8abbf91c439a6
etothej <erikjan.vankesteren@gmail.com>
2016-08-11 20:28:36 +0200

    final 1

--- assets/js/euclidian.js
+++ assets/js/euclidian.js
@@ -1,16 +1,3 @@
-var calculate = function(){
-
-  var var1 = parseInt($("#var1").val()) || 0
-  var var2 = parseInt($("#var2").val()) || 0
-  var var3 = parseInt($("#var3").val()) || 0
-  var var4 = parseInt($("#var4").val()) || 0
-
-  var sum = var1+var2+var3+var4
-
-  $("#output-block").html("<h1>"+sum+"</h1>");
-}
-
-
 var euclid = function(){
   dataset = json;
   var input = [parseInt($("#var1").val()) || 0,
@@ -23,18 +10,25 @@ var euclid = function(){
     var compare = $.map(json[i], function(el) { return(el) }).slice(1);
     dataset[i].dist = Math.sqrt(math.dot(input,compare));
   };
-  console.log(dataset);
+
+  var closest = dataset.sort(function(a,b){return a.dist-b.dist})[1];
+
+  $("#output-block").html("")
+    .append("<h1>"+htmltext+closest.name+"</h1>")
+    .append("<p>"+closest+"</p>");
 }
-
-
-$("#var1").on("change", euclid);
-$("#var2").on("change", calculate);
-$("#var3").on("change", calculate);
-$("#var4").on("change", calculate);
 
 var json;
 var dataset;
 var out;
+
+var htmltext = "The closest value to this is: ";
+
+$("#var1").on("change", euclid);
+$("#var2").on("change", euclid);
+$("#var3").on("change", euclid);
+$("#var4").on("change", euclid);
+
 $.get("assets/JSON/euclid.txt", function(data){
   json = $.parseJSON(data);
   out = new Array(json.length).fill(0);
